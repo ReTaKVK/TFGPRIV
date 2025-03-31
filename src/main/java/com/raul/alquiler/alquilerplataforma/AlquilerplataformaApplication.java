af16650f902a4d7a45d5d6cc739abbb61c444628
Raul <carrascosaraul@gmail.com>
2025-03-31 10:33:17 +0200

    prueba

---
+++ src/main/java/com/raul/alquiler/alquilerplataforma/AlquilerplataformaApplication.java
@@ -0,0 +1,13 @@
+package com.raul.alquiler.alquilerplataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerplataformaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlquilerplataformaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raul/alquiler/alquilerplataforma/AlquilerplataformaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.alquiler.alquilerplataforma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerplataformaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
