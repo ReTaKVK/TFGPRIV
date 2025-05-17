b96732f833c4ea40f10daef37322320921cb4f7e
Raul <carrascosaraul@gmail.com>
2025-05-17 10:19:56 +0200

    arreglo src de perfil

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -98,7 +98,7 @@ function setupNavButtons() {
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                     <li><a class="dropdown-item" href="perfil.html"><i class="bi bi-person me-2"></i>Mi Perfil</a></li>
-                    <li><a class="dropdown-item" href="bookings.html"><i class="bi bi-calendar-check me-2"></i>Mis Reservas</a></li>
+                    <li><a class="dropdown-item" href="perfil.html"><i class="bi bi-calendar-check me-2"></i>Mis Reservas</a></li>
         `;
 
         if (user.rol === "ADMIN") {
